d5b6fa3fdd6c4f2fa37fa28439eb1937504e9c4b
Adrian De Palma <adrian.depalma@servexternos.repsol.com>
2018-11-16 01:13:56 +0100

    working typescript vue and express

--- src/server.ts
+++ src/server.ts
@@ -1,42 +1,19 @@
-/* app/server.ts */
-/*
 // Import everything from express and assign it to the express variable
-import  express from 'express';
+import express from 'express';
 
 // Import WelcomeController from controllers entry point
 import {WelcomeController} from './controllers';
-
-
 
 // Create a new express application instance
 const app: express.Application = express();
 // The port the express app will listen on
 const port: number = 3000;
-
-
+app.use(express.static(__dirname + '/../dist'));
+// Mount the WelcomeController at the /welcome route
 app.use('/welcome', WelcomeController);
-console.log(__dirname );
-app.use(express.static(__dirname + '/dist'));
+
 // Serve the application at the given port
 app.listen(port, () => {
     // Success callback
     console.log(`Listening at http://localhost:${port}/`);
-});*/
-
-const express = require('express');
-const app = express();
-const morgan = require('morgan');
-
-//Setting
-app.set('port', process.env.PORT || 3000 )
-//midelsware
-app.use(morgan('dev'));
-//routes
-
-//static files
-app.use(express.static(__dirname + '/../dist'));
-
-console.log(__dirname );
-app.listen(app.get('port'), () => {
-    console.log('listen on port '+ app.get('port'));
 });
